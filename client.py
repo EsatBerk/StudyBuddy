0bdd1c68362a8cf085bdeb61c8c6b229ec1a9dee
EbrarMutlu <71319071+EbrarMutlu@users.noreply.github.com>
2022-05-24 14:11:31 +0300

    Update client.py
    s yerine socket adını client yazdım
    server'ın gönderdiği mesajı okuyor //ama dediğim gibi keşke file göndermeye başlamadan okusa
    hash olaylarını kaldırdım

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import tqdm
 import os
 import tkinter as tk
 from tkinter import filedialog
-import hashlib
+
 
 BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
@@ -16,23 +16,25 @@ file_path = filedialog.askopenfilename()
 
 file_path= file_path.encode()
 
-print("SHA-512:", hashlib.sha512(file_path).hexdigest())
 
-host = "192.168.0.16"
+
+host = "192.168.1.30"
 port = 5001
 
 
 filesize =  os.path.getsize(file_path)
 
-s = socket.socket()
+client = socket.socket()
 
 print(f"[+] Connecting to {host}:{port}")
 
-s.connect((host,port))
+client.connect((host, port))
 print("[+] Connected. ")
+msg = client.recv(1024)
+print(msg.decode("utf-8"))
 
 
-s.send(f"{file_path}{SEPARATOR}{filesize}".encode())
+client.send(f"{file_path}{SEPARATOR}{filesize}".encode())
 
 progress = tqdm.tqdm(range(filesize), f"Sending {file_path}")
 with open (file_path,"rb") as f:
@@ -42,7 +44,8 @@ with open (file_path,"rb") as f:
 
                         break
 
-                s.sendall(bytes_read)
+                client.sendall(bytes_read)
                 progress.update(len(bytes_read))
 
-s.close()
+print("Successful")
+client.close()
